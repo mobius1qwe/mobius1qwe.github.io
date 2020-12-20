3fbfc2e2d8f97d3a1b940e0891de88504cb469c7
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2020-12-20 14:53:43 -0300

    Project: REST DataWare Wiki Changes:   - Teste de novo Layout

---
+++ _translations.js
@@ -0,0 +1,11 @@
+
+function hnd_ut(a){
+a.TRANSLATIONS['Search term too short'] = "Termo de pesquisa curto demais";
+a.TRANSLATIONS['No results'] = "Sem resultados";
+a.TRANSLATIONS['Please enter 3 or more characters'] = "Por favor digite 3 ou mais caracteres";
+a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "Dicionário ainda não está pronto, aguarde uns instantes até concluir a transferência.";
+a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Houve um erro na pesquisa. Consulte o administrador se deseja saber mais";
+a.TRANSLATIONS['Related topics...'] = "Tópicos relacionados...";
+a.TRANSLATIONS['Loading...'] = "Carregando...";
+a.TRANSLATIONS['Close'] = "Fechar";
+}
